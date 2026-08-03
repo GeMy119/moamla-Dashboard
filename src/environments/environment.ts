8bd69be156341d7fe97bbf613089be33163d0f02
GeMy119 <mahmoud.gamal1191998@gmail.com>
2026-08-03 03:11:13 +0300

    fix error in moamla type html

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,6 @@ export const environment = {
     production: false,
     apiUrl: 'http://localhost:4000/api/v1',
 };
-// export const environment = {
-//     production: true,
-//     apiUrl: 'https://api.servicc-sa.com/api/v1',
-// };
 
 
 
