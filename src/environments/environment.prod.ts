bb68c561c0133343b28330d2c8b36ddcc53496e4
GeMy119 <mahmoud.gamal1191998@gmail.com>
2026-08-19 19:48:32 +0300

    edit api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@
 
 export const environment = {
     production: true,
-    apiUrl: 'https://api.service-sa.online/api/v1',
+    // apiUrl: 'https://api.service-sa.online/api/v1',
+    apiUrl: 'https://moamla-back-end.vercel.app/api/v1',
 };
